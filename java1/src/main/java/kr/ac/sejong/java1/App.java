2419394dbb9dcca29bc1436e606daaab396da5fb
HongEunho <46186664+HongEunho@users.noreply.github.com>
2020-03-24 01:41:30 +0900

    AppPrograming 2week

---
+++ java1/src/main/java/kr/ac/sejong/java1/App.java
@@ -0,0 +1,14 @@
+package kr.ac.sejong.java1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) { //ctrl+shift+f
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("!");
+		
+	}
+}
